1fcf506214363bfc76f62686448dd04fdce157ba
pvadrevu@gmail.com <pvadrevu@gmail.com@1fd40930-cc92-8e8e-0c23-a1e2d0b2de26>
2013-09-17 22:31:25 +0000

    Corrected a bug in ip2asn.py, cleaned up the code

--- amico_scripts/ip2asn.py
+++ amico_scripts/ip2asn.py
@@ -3,7 +3,7 @@
 # pvadrevu@uga.edu                                                        #
 #                                                                         #
 # Distributed under the GNU Public License                                #
-# http://www.gnu.org/licenses/gpl.txt                                     #   
+# http://www.gnu.org/licenses/gpl.txt                                     #
 #                                                                         #
 # This program is free software; you can redistribute it and/or modify    #
 # it under the terms of the GNU General Public License as published by    #
@@ -16,30 +16,22 @@ import sys
 import psycopg2
 import socks
 import socket
-import urllib2
-import re
 import time
-import config
-import hashlib
 import subprocess
+
+import util
 from config import *
 
-# Reverse the IP address for querying origin.asn.cymru.com 
-def reverse_ip(ip):
-    ipreg = re.compile("([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$")
-    m = ipreg.match(ip)
-    if m is not None:
-        return m.group(4)+"."+m.group(3)+"."+m.group(2)+"."+m.group(1) 
 
 USER_AGENT = "Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)"
-CYMRU_TIMEOUT = 1 # Timeout for cymru dig call
+CYMRU_TIMEOUT = 1  # Timeout for cymru dig call
 
 # Connect to database
 try:
     conn = psycopg2.connect("dbname=%s host=%s user=%s password=%s"
-        %(db_name,db_host,db_user,db_password))
+        % (db_name, db_host, db_user, db_password))
 except:
-    print "Unable to connect to database: "+db_name
+    print "Unable to connect to database: " + db_name
 
 # Use Autocommit mode for database connection
 conn.set_isolation_level(0)
@@ -47,24 +39,25 @@ cursor = conn.cursor()
 
 # Setup SOCKS proxy
 if socks_proxy_host:
-    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5,socks_proxy_host,socks_proxy_port);
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5,
+                    socks_proxy_host, socks_proxy_port)
     socket.socket = socks.socksocket
 ###
 
 # Read Arguments
 sha1 = sys.argv[1]
 
-# Database query to get the relevant recent record  
+# Database query to get the relevant recent record
 cursor.execute("""
-    SELECT server, timestamp FROM pe_dumps WHERE sha1 = %s 
-        ORDER BY timestamp DESC;""",(sha1,))
+    SELECT server, timestamp FROM pe_dumps WHERE sha1 = %s
+        ORDER BY timestamp DESC;""", (sha1,))
 row = cursor.fetchone()
 server_ip = row[0]
 
 # Exit if an AS containing this IP has been logged with in the last 1 month
 cursor.execute("""
     SELECT * FROM bgp2asn WHERE log_date > (current_date - interval '1 month')
-    AND bgp_prefix >> %s """,(server_ip,))
+    AND bgp_prefix >> %s """, (server_ip,))
 if cursor.rowcount > 0:
     sys.exit()
 
@@ -81,8 +74,10 @@ if cursor.rowcount > 0:
 #   words.append(word.strip())
 
 # Query asn.cymru.com using dig
-cmd = subprocess.Popen(['dig','+short',reverse_ip(server_ip)+'.origin.asn.cymru.com','TXT'],
-                stdout=subprocess.PIPE)
+# A sample output is:
+#     "701 1239 3549 3561 7132 | 216.90.108.0/24 | US | arin | 1998-09-25"
+cmd = subprocess.Popen(['dig', '+short', util.reverse_ip(server_ip) +
+                '.origin.asn.cymru.com', 'TXT'], stdout=subprocess.PIPE)
 time.sleep(CYMRU_TIMEOUT)
 if cmd.poll() is None:
     cmd.kill()
@@ -91,20 +86,27 @@ as_info = cmd.stdout.readline()
 as_info = as_info.strip().strip('"')
 output = as_info.split('|')
 words = []
-for word in output:
-    words.append(word.strip())
+for answer in output:
+    if answer:
+        words.append(answer.split()[0].strip())
+    else:
+        words.append(None)
+
 #print words
 as_number = words[0]
 bgp_prefix = words[1]
-country_code = words[2] 
+country_code = words[2]
 date_allocated = words[4]
 
-cmd = subprocess.Popen(['dig','+short','AS'+as_number+'.asn.cymru.com','TXT'],
-                stdout=subprocess.PIPE)
+# Sample output:
+#     "23028 | US | arin | 2002-01-04 | TEAMCYMRU - SAUNET"
+cmd = subprocess.Popen(['dig', '+short', 'AS' + as_number + '.asn.cymru.com',
+                        'TXT'], stdout=subprocess.PIPE)
 time.sleep(CYMRU_TIMEOUT)
 if cmd.poll() is None:
     cmd.kill()
-    print "ip2asn.py: Couldn't finish the call to cymru for {0}. Aborting...".format((server_ip,))
+    print ("ip2asn.py: Couldn't finish the call to cymru for {0}. Aborting..."
+            .format((server_ip,)))
     sys.exit()
 as_info = cmd.stdout.readline()
 as_info = as_info.strip().strip('"')
@@ -117,12 +119,13 @@ as_name = words[4]
 
 # Store the record in the database
 cursor.execute("""
-    INSERT INTO bgp2asn(bgp_prefix,as_number,as_name,country_code,date_allocated,log_date)
-    VALUES (%s,%s,%s,%s,%s,current_date)""",(bgp_prefix,as_number,as_name,country_code,
-    date_allocated))
+    INSERT INTO
+        bgp2asn
+        (bgp_prefix, as_number, as_name, country_code,
+         date_allocated, log_date)
+    VALUES (%s,%s,%s,%s,%s,current_date)"""
+    , (bgp_prefix, as_number, as_name, country_code,
+      date_allocated))
 
 cursor.close()
 conn.close()
-
-
-
--- amico_scripts/util.py
+++ amico_scripts/util.py
@@ -3,7 +3,7 @@
 # pvadrevu@uga.edu                                                        #
 #                                                                         #
 # Distributed under the GNU Public License                                #
-# http://www.gnu.org/licenses/gpl.txt                                     #   
+# http://www.gnu.org/licenses/gpl.txt                                     #
 #                                                                         #
 # This program is free software; you can redistribute it and/or modify    #
 # it under the terms of the GNU General Public License as published by    #
@@ -21,14 +21,16 @@ import re
 
 from config import *
 
+
 def connect_to_db():
     try:
         conn = psycopg2.connect("dbname=%s host=%s user=%s password=%s"
-            %(db_name, db_host, db_user, db_password))
+            % (db_name, db_host, db_user, db_password))
     except:
-        print "Unable to connect to database: "+db_name
+        print "Unable to connect to database: " + db_name
     conn.set_isolation_level(0)
     return conn
+
 
 # Reorder the subdomains in the host name such that
 # the TLD comes first. Eg: com.google.www
@@ -48,12 +50,14 @@ def reorder_domain(host):
     else:
         return host
 
+
 def is_ip(string):
     ipreg = re.compile("[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$")
     if ipreg.match(string) is not None:
         return True
     else:
         return False
+
 
 def extract_extension(url):
     file_name = url.split('?')[0].split('/')[-1]
@@ -62,12 +66,23 @@ def extract_extension(url):
         return ext
     else:
         return None
-    
+
+
 def extract_twold(url):
     etld_obj = etld.etld()
     registered = ''
     suffix = ''
     registered, suffix = etld_obj.parse(url)
     twold = '.'.join([registered.split('.')[-1], suffix])
-    #print "twold: ", twold 
+    #print "twold: ", twold
     return twold
+
+
+# Reverse the IP address for querying origin.asn.cymru.com
+def reverse_ip(ip):
+    ipreg = re.compile(
+            "([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$")
+    m = ipreg.match(ip)
+    if m is not None:
+        return (m.group(4) + "." + m.group(3) + "." + m.group(2)
+                + "." + m.group(1))
