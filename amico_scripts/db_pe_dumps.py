74a842e8d8a33dfb5260edda0b11836a66a50bf5
pvadrevu@gmail.com <pvadrevu@gmail.com@1fd40930-cc92-8e8e-0c23-a1e2d0b2de26>
2014-04-04 18:35:37 +0000

    Removed an obsolete script and minor changes to db_pe_dumps.py

--- amico_scripts/db_pe_dumps.py
+++ amico_scripts/db_pe_dumps.py
@@ -12,126 +12,120 @@
 #                                                                         #
 ###########################################################################
 
-import psycopg2
 import re
 import sys
 from config import *
-import socket
 
-import feature_cache
 import util
 
-#print "Time b4 http parsing: %f" %(time.time(),)
-# Use Autocommit mode for database connection
-conn = util.connect_to_db()
-cursor = conn.cursor()
+def db_pe_dumps(file_name, sha1, md5, file_size):
+    #print "Time b4 http parsing: %f" %(time.time(),)
+    # Use Autocommit mode for database connection
+    conn = util.connect_to_db()
+    cursor = conn.cursor()
 
-fileHandle = open(sys.argv[1])
+    fileHandle = open(file_name)
 
-# Timestamp
-r = re.compile('[0-9]+')
-timestamp = r.search(fileHandle.readline())
-if timestamp is not None:
-    timestamp = timestamp.group()
-    #print timestamp.group()
+    # Timestamp
+    r = re.compile('[0-9]+')
+    timestamp = r.search(fileHandle.readline())
+    if timestamp is not None:
+        timestamp = timestamp.group()
+        #print timestamp.group()
 
-# Source and Destination IPs
-r = re.compile('([0-9.]+):.*-([0-9.]+):([0-9]+)-.*')
-ip = r.search(fileHandle.readline())
-if ip is not None:
-    srcip = ip.group(2)
-    dstip = ip.group(1)
-    dst_port = ip.group(3)
-    #print ip.group(1)
-    #print ip.group(2)
-else:
-    srcip = None
-    dstip = None
-    dst_port = None
+    # Source and Destination IPs
+    r = re.compile('([0-9.]+):.*-([0-9.]+):([0-9]+)-.*')
+    ip = r.search(fileHandle.readline())
+    if ip is not None:
+        srcip = ip.group(2)
+        dstip = ip.group(1)
+        dst_port = ip.group(3)
+        #print ip.group(1)
+        #print ip.group(2)
+    else:
+        srcip = None
+        dstip = None
+        dst_port = None
 
-# URL
-r = re.compile('(GET|POST|HEAD) (.*) ')
-url = r.search(fileHandle.readline())
-if url is not None:
-    method = url.group(1)
-    method = method[:10]
-    url = url.group(2)
-    #print url.group(1)
-else:
-    method = None
-
-
-# Host
-r = re.compile('Host: (.*)')
-host = r.search(fileHandle.readline())
-if host is not None:
-    host = host.group(1)
-    host = util.reorder_domain(host.strip())
-    #print host.group(1)
+    # URL
+    r = re.compile('(GET|POST|HEAD) (.*) ')
+    url = r.search(fileHandle.readline())
+    if url is not None:
+        method = url.group(1)
+        method = method[:10]
+        url = url.group(2)
+        #print url.group(1)
+    else:
+        method = None
 
 
-# Referer
-r = re.compile('Referer: (.*)')
-referer = r.search(fileHandle.readline())
-if referer is not None:
-    referer = referer.group(1)
-    #print referrer.group(1)
+    # Host
+    r = re.compile('Host: (.*)')
+    host = r.search(fileHandle.readline())
+    if host is not None:
+        host = host.group(1)
+        host = util.reorder_domain(host.strip())
+        #print host.group(1)
 
 
-# CORRUPT_PE
-corrupt_pe = "FALSE"
-r = re.compile('CORRUPT_PE')
-corrupt_pe_str = r.search(fileHandle.readline())
-if corrupt_pe_str is not None:
-    corrupt_pe = "TRUE"
+    # Referer
+    r = re.compile('Referer: (.*)')
+    referer = r.search(fileHandle.readline())
+    if referer is not None:
+        referer = referer.group(1)
+        #print referrer.group(1)
 
 
-# Now, parse data from the response
-# Server
-data = fileHandle.read()
-r = re.compile('Server: (.*)')
-server = r.search(data)
-if server is not None:
-    server = server.group(1)
-    server = server.rstrip('\r')
-    server = server[:64]
+    # CORRUPT_PE
+    corrupt_pe = "FALSE"
+    r = re.compile('CORRUPT_PE')
+    corrupt_pe_str = r.search(fileHandle.readline())
+    if corrupt_pe_str is not None:
+        corrupt_pe = "TRUE"
 
-# Content-Type
-r = re.compile('Content-Type: (.*)')
-cont_type = r.search(data)
-if cont_type is not None:
-    cont_type = cont_type.group(1)
-    cont_type = cont_type.rstrip('\r')
-    cont_type = cont_type[:128]
 
-sha = sys.argv[2]
-md5 = sys.argv[3]
-file_size = sys.argv[4]
+    # Now, parse data from the response
+    # Server
+    data = fileHandle.read()
+    r = re.compile('Server: (.*)')
+    server = r.search(data)
+    if server is not None:
+        server = server.group(1)
+        server = server.rstrip('\r')
+        server = server[:64]
 
-#print "Time after http parsing: %f" %(time.time(),)
-# Database statement
-cursor.execute("""
-    INSERT INTO pe_dumps(sha1,md5,timestamp,server,client,method,url,host,
-    referer,server_application,content_type,dst_port,corrupt,file_size)
-    VALUES
-    (%s,%s,TO_TIMESTAMP(%s),%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
-    (sha, md5, timestamp, srcip, dstip, method, url, host, referer, server,
-    cont_type, dst_port, corrupt_pe, file_size))
-cursor.execute("""
-    SELECT dump_id FROM pe_dumps where sha1 = %s ORDER BY dump_id DESC
-    """, (sha,))
-dump_id = cursor.fetchone()[0]
-print ("A new entry on host:%s has been made in pe_dumps table with "
-      "dump_id %s" % (host, dump_id))
+    # Content-Type
+    r = re.compile('Content-Type: (.*)')
+    cont_type = r.search(data)
+    if cont_type is not None:
+        cont_type = cont_type.group(1)
+        cont_type = cont_type.rstrip('\r')
+        cont_type = cont_type[:128]
 
-# Disabled feature cache and calling feature generation script from
-# process_dump.sh
-"""
-if dump_id is not None:
-    if feature_cache.check_feature_exists(cursor, dump_id, host, sha):
-        feature_cache.copy_features(cursor, dump_id, host, sha)
-"""
+    #print "Time after http parsing: %f" %(time.time(),)
+    # Database statement
+    cursor.execute("""
+        INSERT INTO pe_dumps(sha1,md5,timestamp,server,client,method,url,host,
+        referer,server_application,content_type,dst_port,corrupt,file_size)
+        VALUES
+        (%s,%s,TO_TIMESTAMP(%s),%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
+        (sha1, md5, timestamp, srcip, dstip, method, url, host, referer, server,
+        cont_type, dst_port, corrupt_pe, file_size))
+    cursor.execute("""
+        SELECT dump_id FROM pe_dumps where sha1 = %s ORDER BY dump_id DESC
+        """, (sha1,))
+    dump_id = cursor.fetchone()[0]
+    print ("A new entry on host:%s has been made in pe_dumps table with "
+          "dump_id %s" % (host, dump_id))
 
-fileHandle.close()
-cursor.close()
-conn.close()
+    fileHandle.close()
+    cursor.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    file_name = sys.argv[1]
+    sha1 = sys.argv[2]
+    md5 = sys.argv[3]
+    file_size = sys.argv[4]
+    db_pe_dumps(file_name, sha1, md5, file_size)
