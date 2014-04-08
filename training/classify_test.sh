58e1dc3085f36f160b4a42eb676969297b9f9ee9
pvadrevu@gmail.com <pvadrevu@gmail.com@1fd40930-cc92-8e8e-0c23-a1e2d0b2de26>
2014-04-08 03:54:34 +0000

    Removed old training dir.

--- training/classify_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-dir=models
-java -Xmx2000m -cp /usr/share/java/weka.jar weka.classifiers.meta.FilteredClassifier -t $dir/train.arff -d $dir/rf.model -p 1,58,59 -distribution -F "weka.filters.unsupervised.attribute.RemoveType -T string" -W weka.classifiers.trees.RandomForest -- -K 0 -S 1 -I 50 
-echo "Finished setting up testing"
--- training/config.py
+++
@@ -1,4 +0,0 @@
-db_host="localhost"
-db_name=""
-db_user=""
-db_password=""
