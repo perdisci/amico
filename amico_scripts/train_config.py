3ec8213c5f1cff75e906842407af159a1a37b3f9
pvadrevu@gmail.com <pvadrevu@gmail.com@1fd40930-cc92-8e8e-0c23-a1e2d0b2de26>
2014-04-15 14:50:22 +0000

    Comments added

--- amico_scripts/train_config.py
+++ amico_scripts/train_config.py
@@ -1,8 +1,11 @@
-# Example: training_start_date = "2014-01-01"  
-# If left as None, all the download events from the beginning are 
+# Example: training_start_date = "2014-02-30"
+# If left as None, all the download events from the beginning are
 # used for training the classifier
-training_start_date = None
+# It is recommended to leave atleast 30 days for bootstrapping i.e.
+# say you started running AMICO from 2014-01-20, then the training
+# start date should be later than 2014-02-20
+training_start_date = "2012-08-01"
 # If left as None, all the download events till the end are used
 # for training the classifier
 # training_days = 60
-training_days = None
+training_days = 60
