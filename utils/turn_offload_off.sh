7707494d3325469ff0f36c13b518594c478d2b9b
roberto.perdisci@gmail.com <roberto.perdisci@gmail.com@1fd40930-cc92-8e8e-0c23-a1e2d0b2de26>
2014-08-21 15:44:24 +0000

    turn off more nic features that may negatively impact performance

--- utils/turn_offload_off.sh
+++ utils/turn_offload_off.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# run as root...
+
 NIC=$1
 
 ethtool -K $NIC tso off
-ethtool -K $NIC gso off
 ethtool -K $NIC gro off
+ethtool -K $NIC lro off
+ethtool -K $NIC gso off
+ethtool -K $NIC rx off
+ethtool -K $NIC tx off
+ethtool -K $NIC sg off
